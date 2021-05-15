d513f885fae392e18f629d76e0949b0c47e64169
T__R__P__53 <tyler_1701@hotmail.com>
2021-05-15 11:08:35 -0400

    styling

--- client/src/components/Crypto/crypto.js
+++ client/src/components/Crypto/crypto.js
@@ -1,12 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { AiOutlineReload } from 'react-icons/ai'
 import fetchJSON from '../../utils/API'
 import './crypto.css'
 
 function Crypto (){
 
-    const [info, setInfo] = useState([])
+    const [info, setInfo] = useState([{
+        name: 'bitcoin', 
+        price: {
+            price: 48000.85,
+            percent_change_1h: 0.15,
+            percent_change_24h: -16.14,
+            percent_change_7d: -2.90,
+            percent_change_30d: -21.65,
+        }
+    }])
     const [displayedList, setDisplayedList] = useState({
-        results: [],
+        results: [{
+            name: 'bitcoin', 
+            price: {
+                price: 48000.85,
+                percent_change_1h: 0.15,
+                percent_change_24h: -16.14,
+                percent_change_7d: -2.90,
+                percent_change_30d: -21.65,
+            }
+        }],
         sortOrder: ""
     })
     const cryptoInput = useRef()
@@ -14,15 +34,18 @@ function Crypto (){
     async function getData(){
         const temp = []
 
-        const data = await fetchJSON('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest')
-        console.log(data)
-        data.data.forEach(a=>
-            setInfo(info => [...info, {name: a.name.toLowerCase(), price: a.quote.USD, tags: a.tags}])
-            )
-        data.data.forEach(a=>
-            temp.push({name: a.name.toLowerCase(), price: a.quote.USD, tags: a.tags}))
-        setDisplayedList({results: temp})
+        // const data = await fetchJSON('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest')
+        // data.data.forEach(a=>
+        //     setInfo(info => [...info, {name: a.name.toLowerCase(), price: a.quote.USD, tags: a.tags}])
+        //     )
+        // data.data.forEach(a=>
+        //     temp.push({name: a.name.toLowerCase(), price: a.quote.USD, tags: a.tags}))
+        // setDisplayedList({results: temp})
     }
+
+    useEffect(() => {
+        getData()
+    }, [])
 
     function submitSearch(event){
         event.preventDefault()
@@ -41,21 +64,23 @@ function Crypto (){
     }
 
     function reset(){
+        getData()
         setDisplayedList ({results: info})
     }
-
-    useEffect(() => {
-        getData()
-    }, [])
 
     return (
         <>
         <div className="App">
-            <input className="cryptoSearchInput" placeholder="Search Crypto" ref={cryptoInput} onKeyDown={submit.bind(this)}/>
-            <button onClick={submitSearch}>search</button>
-            <button onClick={reset}>Reset</button>
-            <div className="cryptoContainer">
-                
+        <div className="cryptoContainer">
+            <div className="searchCont">
+            <div>
+                <input className="cryptoSearchInput" spellCheck='false' placeholder="Search Crypto" ref={cryptoInput} onKeyDown={submit.bind(this)}/>
+                <button className="cryptoSearchBtn" onClick={submitSearch}><FaSearch/></button>
+            </div>
+            <div>
+                <button className="cryptoResetBtn" onClick={reset}><AiOutlineReload className="resetIcon"/></button>
+            </div>
+            </div>
                 {displayedList.results.length > 0 ?
                 <table>
                 <thead>
@@ -72,12 +97,12 @@ function Crypto (){
                     return (
                         <tbody key={idx}>
                             <tr>
-                                <td>{crypto.name.charAt(0).toUpperCase() + crypto.name.slice(1)}</td>
+                                <td className="tdName">{crypto.name.charAt(0).toUpperCase() + crypto.name.slice(1)}</td>
                                 <td style={{color: 'white', opacity: '0.5'}}>${crypto.price.price.toFixed(2)} USD</td>
-                                <td style={{color: Math.sign(crypto.price.percent_change_1h) != -1 ? 'green' : 'red'}}>{crypto.price.percent_change_1h.toFixed(2)}%</td>
-                                <td style={{color: Math.sign(crypto.price.percent_change_7d) != -1 ? 'green' : 'red'}}>{crypto.price.percent_change_7d.toFixed(2)}%</td>
-                                <td style={{color: Math.sign(crypto.price.percent_change_24h) != -1 ? 'green' : 'red'}}>{crypto.price.percent_change_24h.toFixed(2)}%</td>
-                                <td style={{color: Math.sign(crypto.price.percent_change_30d) != -1 ? 'green' : 'red'}}>{crypto.price.percent_change_30d.toFixed(2)}%</td>
+                                <td style={{color: Math.sign(crypto.price.percent_change_1h) != -1 ? 'rgb(0, 250, 0, 0.9)' : 'red'}}>{crypto.price.percent_change_1h.toFixed(2)}%</td>
+                                <td style={{color: Math.sign(crypto.price.percent_change_7d) != -1 ? 'rgb(0, 250, 0, 0.9)' : 'red'}}>{crypto.price.percent_change_7d.toFixed(2)}%</td>
+                                <td style={{color: Math.sign(crypto.price.percent_change_24h) != -1 ? 'rgb(0, 250, 0, 0.9)' : 'red'}}>{crypto.price.percent_change_24h.toFixed(2)}%</td>
+                                <td style={{color: Math.sign(crypto.price.percent_change_30d) != -1 ? 'rgb(0, 250, 0, 0.9)' : 'red'}}>{crypto.price.percent_change_30d.toFixed(2)}%</td>
                             </tr>
                         </tbody>
                     )
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,6 @@ function fetchJSON( url, method='get', data={} ){
         method,
         headers: {
             'Content-Type': 'application/json',
-            'X-CMC_PRO_API_KEY': 'b843b898-af9d-444c-b7bd-e6a955e842b8'
         }
     }
     if( method === 'post' || method === 'put' ) {
